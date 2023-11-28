24a8d622978c7d4608246c9aad50901fe45df805
RicTieri <tieri.riccardo@gmail.com>
2023-11-28 16:18:49 +0100

    added custom image

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ const posts = [
     {
         "id": 2,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "./DSC_7526.jpg",
+        "author": {
+            "name": "Riccardo Tieri",
+            "image": "./DSC_5069.jpg"
+        },
+        "likes": 5,
+        "created": "2021-03-05"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=112",
         "author": {
             "name": "Sofia Perlari",
@@ -22,7 +33,7 @@ const posts = [
         "created": "2021-09-03"
     },
     {
-        "id": 3,
+        "id": 4,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=234",
         "author": {
@@ -33,18 +44,29 @@ const posts = [
         "created": "2021-05-15"
     },
     {
-        "id": 4,
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "./DSC_5574.jpg",
+        "author": {
+            "name": "Riccardo Tieri",
+            "image": "./DSC_5069.jpg"
+        },
+        "likes": 115,
+        "created": "2021-03-05"
+    },
+    {
+        "id": 6,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": "https://unsplash.it/300/300?image=20"
+            "image": null
         },
         "likes": 56,
         "created": "2021-04-03"
     },
     {
-        "id": 5,
+        "id": 7,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=534",
         "author": {
@@ -53,27 +75,45 @@ const posts = [
         },
         "likes": 95,
         "created": "2021-03-05"
-    }
+    },
+    {
+        "id": 8,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "./DSC_5574.jpg",
+        "author": {
+            "name": "Riccardo Tieri",
+            "image": "./DSC_5069.jpg"
+        },
+        "likes": 95,
+        "created": "2023-05-05"
+    },
+
 ];
 
 const container = document.getElementById('container');
 
 posts.forEach((post) => {
     container.innerHTML += createPostFrom(post);
+    if (post.author.image == null) {
+        document.querySelector('.post-meta__icon-' + post.id).innerHTML = '';
+        document.querySelector('.post-meta__icon-' + post.id).innerHTML = profilePicDefault(post.author.name);
+    }
 })
+
+const likedPosts = [];
 
 for (let i = 0; i < posts.length; i++) {
     let likeBtn = document.querySelector('a[data-postid="' + posts[i].id + '"]');
-    let likeCounter = document.querySelector('#like-counter-'+[i]);
     let clicked = false;
+    let likeCounter = document.querySelector('b#like-counter-' + [i]);
+    likeCounter.innerHTML = i
     likeBtn.addEventListener('click', function () {
         if (!clicked) {
             likeBtn.classList.toggle('like-button--liked');
-            clicked = true; 
-            likeCounter.innerText = 1;
+            clicked = true;
+            likedPosts.push({ post_id: posts[i].id });
         } else {
             clicked = false;
-         
         }
     })
 }
@@ -87,7 +127,7 @@ function createPostFrom(obj) {
     <div class="post">
         <div class="post__header">
             <div class="post-meta">                    
-                <div class="post-meta__icon">
+                <div class="post-meta__icon-${obj.id}">
                     <img class="profile-pic" src="${obj.author.image}" alt="${obj.author.name}">                    
                 </div>
                 <div class="post-meta__data">
@@ -113,6 +153,18 @@ function createPostFrom(obj) {
                 </div>
             </div> 
         </div>            
+    </div>
+    `
+}
+
+
+function profilePicDefault(userName) {
+    let nameParts = userName.split(' ');
+    let sign = nameParts[0].charAt(0) + nameParts[1].charAt(0);
+    console.log(sign)
+    return `
+    <div class="profile-pic-default">
+        <span>${sign}</span>
     </div>
     `
 }
