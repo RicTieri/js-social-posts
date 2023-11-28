28acdda92a59cb521840375f9fd68187ba635b34
RicTieri <tieri.riccardo@gmail.com>
2023-11-28 14:38:37 +0100

    added dom output

--- main.js
+++ main.js
@@ -55,3 +55,48 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById('container');
+
+posts.forEach((post) => {
+    container.innerHTML += createPostFrom(post)
+})
+
+
+
+// function
+
+function createPostFrom(obj){
+    return  `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${obj.author.image}" alt="${obj.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${obj.author.name}</div>
+                    <div class="post-meta__time">${obj.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${obj.content}</div>
+        <div class="post__image">
+            <img src="${obj.media}" alt="${obj.author.name}">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${obj.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+}
